34e91d29923e0c567e37e6fc9b560e850b50b9b9
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-03-14 03:05:31 -0500

    first navbar version is ready

--- src/components/Private/load-file/PreviewFile/PreviewFile.tsx
+++ src/components/Private/load-file/PreviewFile/PreviewFile.tsx
@@ -1,6 +1,5 @@
 "use client"
 import styles from './PreviewFile.module.scss';
-import Papa, { ParseResult } from 'papaparse';
 import { useState, ChangeEvent } from 'react';
 import { UploadFileInput } from '../UploadFileInput';
 import { Datatable } from '../DataTable';
@@ -14,19 +13,6 @@ const title = "Vista previa del archivo";
 export const PreviewFile = () =>{
     const [parsedData, setParsedData] = useState<CSVRow[]>([]);
 
-    const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file && file.type === 'text/csv') {
-            Papa.parse(file, {
-                header: true,
-                dynamicTyping: true,
-                complete: (results: ParseResult<CSVRow>) => {
-                    setParsedData(results.data);
-                },
-            });
-        }
-    };
-
     const handleUpload = async (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         const type= "csv"
@@ -37,19 +23,18 @@ export const PreviewFile = () =>{
 
         const formData = new FormData();
         formData.append("file", file);
-        //formData.append("type", type); // Parámetro adicional
+        formData.append("type", type);
 
         try {
             console.log('FormData being sent:', formData);
-            const res = await fetch(`https://backend-audit-ai.onrender.com/convertToJson?type=${type}`, {
+            const res = await fetch(`https://backend-audit-ai.onrender.com/convertToJson`, {
                 method: "POST",
-                body: formData, // Enviar como FormData
+                body: formData,
             });
 
             if (!res.ok) throw new Error("Error al subir el archivo");
 
-            const data = await res.json(); // Convertir la respuesta a JSON
-            console.log(data);
+            const data = await res.json();
             setParsedData(data);
         } catch (error) {
             console.error("Error:", error);
@@ -57,7 +42,6 @@ export const PreviewFile = () =>{
         }
     };
 
-    
     return(
         <>
             <UploadFileInput onChange={handleUpload}/>
