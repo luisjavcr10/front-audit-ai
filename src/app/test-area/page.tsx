3a596fdaa28dbb0ae8d06e8ebad469496ded65d6
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-03-15 23:41:03 -0500

    quitamos tailwind

--- src/app/test-area/page.tsx
+++ src/app/test-area/page.tsx
@@ -1,10 +1,8 @@
-import { Dashboard } from "@/components/Dashboard";
 import styles from './page.module.scss';
 
 export default function Test (){
     return(
         <div className={styles.page}>
-            <Dashboard/>
         </div>
     );
 }
