1207bc949545a1fd62df7f3084bab0cf4705c922
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-03-15 19:33:51 -0500

    quitar fonts en nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,7 @@
-import type { NextConfig } from "next";
-
-export const nextConfig: NextConfig = {
-  async headers() {
-      return [
-          {
-              source: "/(.*)",
-              headers: [
-                  {
-                      key: "Content-Security-Policy",
-                      value: `
-                          default-src 'self';
-                          script-src 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com;
-                          style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-                          font-src 'self' https://fonts.gstatic.com https://fonts.sandbox.google.com https://docs.google.com;
-                          img-src 'self' data: https://*.googleusercontent.com;
-                          connect-src 'self' https://www.googleapis.com;
-                          frame-src 'self' https://accounts.google.com;
-                      `.replace(/\s+/g, " "),
-                  },
-              ],
-          },
-      ];
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig: import('next').NextConfig = {
 };
+
+module.exports = nextConfig
