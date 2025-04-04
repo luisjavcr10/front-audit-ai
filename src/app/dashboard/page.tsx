e8b6e23d44d8bcdde1afb582c8a6b914e93f7bcc
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-04-03 23:42:00 -0500

    go to dashbaord isready

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,34 @@
 "use client"
 import styles from './page.module.scss';
 import Dashboard from "@/components/Shared/Dashboard/Dashboard"
-import { useEffect } from 'react';
+import { useState,useEffect } from 'react';
+
+interface Grafic {
+  title: string;
+  typeGrafic: string;
+  chartData: ChartData;
+  description: string;
+}
+interface ChartData {
+  labels?: string[];
+  datasets: Array<{
+    label?: string;
+    data: number[] | {x: number, y: number, r?: number}[];
+    backgroundColor?: string | string[];
+    borderColor?: string | string[];
+    borderWidth?: number;
+    tension?: number;
+    fill?: boolean;
+  }>;
+}
 
 export default function Page() {
+  const [grafics, setGrafics] = useState<Grafic[]>([]); 
+
   useEffect(() => {
     const data = sessionStorage.getItem('tempDashboardData');
     if (data) {
+        setGrafics(JSON.parse(data));
         console.log(JSON.parse(data));
         sessionStorage.removeItem('tempDashboardData');
     }
@@ -14,7 +36,7 @@ export default function Page() {
 
   return (
     <div className={styles.page}>
-      <Dashboard/>
+      <Dashboard grafics={grafics}/>
     </div>
   )
 }
