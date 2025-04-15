dc0fbe4cb1151995430aa56ce4cc386cbfe2c465
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-04-15 16:25:11 -0500

    login and regiter not available

--- src/components/Private/register/FormRegister/FormRegister.tsx
+++ src/components/Private/register/FormRegister/FormRegister.tsx
@@ -1,34 +1,25 @@
-"use client";
 import styles from './FormRegister.module.scss';
-
-import { useState } from 'react';
 
 import { InputForm } from "@/components/Shared/InputForm";
 import { ButtonForm } from '@/components/Shared/ButtonForm';
-import { ErrorToast } from '@/components/Shared/ErrorToast';
 
 import { emailForm, passwordForm, repeatPasswordForm } from '@/constants/formData';
 
 export const FormRegister = () =>{
-    const [error, setError] = useState<string | null>(null);
-
-    const handleRegister = () =>{
-        console.log('register');
-    }
 
     return(
         <>
-            <form onSubmit={handleRegister} className={styles.FormRegister}>
+            <form className={styles.FormRegister}>
                 <InputForm type={emailForm}/>
                 <InputForm type={passwordForm}/>
                 <InputForm type={repeatPasswordForm}>
                     <p className={styles.FormRegister__PasswordDoenstMatch}>
                         Repeat password does not match
                     </p>
-                </InputForm>
-                <ButtonForm text='Register' type='local' />
+                </InputForm> 
             </form>
-            {error && <ErrorToast errorMessage={error} onClose={()=>setError(null)}/>}
+            {/** Cuando este listo el login, colocar el boton dentro del form */}
+            <ButtonForm text='Register' type='local' />
         </>
         
     )
