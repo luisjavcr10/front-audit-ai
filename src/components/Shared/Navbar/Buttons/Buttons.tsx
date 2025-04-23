c26064124d573bc2da9b3458bae82b9aca0d7f50
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-04-23 10:06:38 -0500

    added user in navbar

--- src/components/Shared/Navbar/Buttons/Buttons.tsx
+++ src/components/Shared/Navbar/Buttons/Buttons.tsx
@@ -1,13 +1,23 @@
 import styles from '../Navbar.module.scss';
 import { ThemeButton } from '../ThemeButton';
 import Link from 'next/link';
+import { useAuth } from '@/context/AuthContext';
+import { FaUserCircle } from "react-icons/fa";
 
 export const Buttons = () =>{
+    const {isAuthenticated} = useAuth();
     return(
         <>
             <ThemeButton/>
-            <Link href='/login' className={styles.Navbar__RightSection__Access}>Access</Link>
-            <Link href='/register' className={styles.Navbar__RightSection__Register}>Register</Link>
+            {isAuthenticated ? 
+            <FaUserCircle className={styles.Navbar__RightSection__Icon}/>
+            :
+            <>
+                <Link href='/login' className={styles.Navbar__RightSection__Access}>Access</Link>
+                <Link href='/register' className={styles.Navbar__RightSection__Register}>Register</Link>
+            </>
+            }
+            
         </>
     );
 }
