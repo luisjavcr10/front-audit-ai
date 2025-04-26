59fe4e06c52423bd1dfd69d78d5d713fe465b2d2
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-04-25 19:39:22 -0500

    added upload file for login

--- src/components/Private/upload-file/UploadFileInput/UploadFileInput.tsx
+++ src/components/Private/upload-file/UploadFileInput/UploadFileInput.tsx
@@ -1,17 +1,33 @@
+"use client";
 import styles from "./UploadFileInput.module.scss";
 import { FaGoogleDrive } from "react-icons/fa";
+import { useAuth } from "@/context/AuthContext";
 
-export const UploadFileInput = ({ onChangeLocal, googleDrive }: Readonly<{ onChangeLocal: React.ChangeEventHandler; googleDrive: () => void }>) => {
+export const UploadFileInput = ({ 
+    onChangeLocal, googleDrive 
+}: Readonly<{ 
+    onChangeLocal: React.ChangeEventHandler; googleDrive: () => void 
+}>) => {
+    const {isAuthenticated} = useAuth();
+
     return (
         <div className={styles.UploadFileInput}>
             <div className={styles.UploadFileInput__ButtonsSection}>
                 <input type="file" id="file-upload" accept=".csv" onChange={onChangeLocal} />
 
-                <label htmlFor="file-upload" className={styles["custom-file-upload"]}>
-                    Select file <p className={styles.UploadFileInput__Types}>(CSV, Excel, Numbers)</p>
+                <label 
+                    htmlFor="file-upload" 
+                    className={`${styles["custom-file-upload"]} ${!isAuthenticated? styles.UploadFileInput__Hide:''}`}>
+                    Select file 
+                    <p className={styles.UploadFileInput__Types}>
+                        (CSV, Excel, Numbers)
+                    </p>
                 </label>
 
-                <button aria-label="api-google-drive" onClick={googleDrive} className={styles.UploadFileInput__ButtonsSection__DriveButton}>
+                <button 
+                    aria-label="api-google-drive" 
+                    onClick={googleDrive} 
+                    className={`${styles.UploadFileInput__ButtonsSection__DriveButton} ${!isAuthenticated? styles.UploadFileInput__Hide:''}`}>
                     <FaGoogleDrive className={styles.UploadFileInput__ButtonsSection__DriveButton__Icon}/>
                 </button>
             </div>
